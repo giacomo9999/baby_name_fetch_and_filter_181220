135b8b4bae5e5f2f6ddfce590fcb78e3d83bfc9e
James Gary <gmarchesi888@gmail.com>
2018-12-18 14:52:06 -0500

    Started building components.

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Contact.css";
+
+function Contact(props) {
+  return (
+    <div>
+      <span>
+        <p>Lindsay Brown</p>
+      </span>
+    </div>
+  );
+}
+
+export default Contact;
