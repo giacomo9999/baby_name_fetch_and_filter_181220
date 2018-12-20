bbb45d24e2f4b860e1c1d5508dd6ae8e0e440e1d
James Gary <gmarchesi888@gmail.com>
2018-12-20 07:54:58 -0500

    more renaming.

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
+
+function List(props) {
+  console.log("Now I'm in List");
+  // let strOut=props.name+" "+props.race+" "+props.year;
+  let strOut="list goes here";
+  return (
+    <div className="list">
+      <span>{strOut}</span>
+    </div>
+  );
+}
+
+List.propTypes = { name: PropTypes.string.isRequired };
+
+export default List;
--- src/components/ListOfLists.js
+++ src/components/ListOfLists.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Contact from "./Contact";
+import List from "./List";
 
 function ListOfLists(props) {
   console.log("Now I'm in ContactList");
   return (
     <div>
       <h2>-List Of Lists-</h2>
+      <List />
       {/* {props.contacts.map(c => (
-        <Contact key={c.key} race={c.race} name={c.name} year={c.birthYear} />
+        <List key={c.key} race={c.race} name={c.name} year={c.birthYear} />
       ))} */}
     </div>
   );
